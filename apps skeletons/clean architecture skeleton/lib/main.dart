d9cc9ad5071d83710f8ec558d1da1207c6e4848f
Salah Alshafey <salahalshafey@gmail.com>
2022-09-29 17:07:15 +0200

    adding

--- apps skeletons/clean architecture skeleton/lib/main.dart
+++ apps skeletons/clean architecture skeleton/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_config/flutter_config.dart';
 
 import 'src/injection_container.dart' as di;
 
@@ -8,8 +7,6 @@ import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await FlutterConfig.loadEnvVariables();
 
   await Firebase.initializeApp();
 
